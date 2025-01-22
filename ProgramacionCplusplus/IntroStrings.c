b7cfb2009691273c08300c80f9a8178178b2d298
Alberto Sanchez <asanchez@usfq.edu.ec>
2025-01-22 10:29:51 -0500

    first commit

---
+++ ProgramacionCplusplus/IntroStrings.c
@@ -0,0 +1,18 @@
+/*
+Programa para ingresar e imprimir strings
+en consola.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char nombre[80];
+    char apellido[80];
+    
+    printf("Ingresa tu nombre y apellido\n");
+    scanf("%s %s",&nombre,&apellido);
+    printf("Tu nombre es: %s %s",nombre,apellido);
+
+    return 0;
+}
---
+++ ProgramacionCplusplus/holamundo.c
@@ -0,0 +1,10 @@
+/*Holamundo.c
+Alberto
+20 enero 2025
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Hola Mundo\n");
+}
