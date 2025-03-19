f8e35b864a184b645138c03d7404ba3c06ea953e
Alberto Sanchez <asanchez@usfq.edu.ec>
2025-03-19 15:50:48 -0500

    hexagono

--- ProgramacionCplusplus/graficos.cpp
+++ ProgramacionCplusplus/graficos.cpp
@@ -18,6 +18,11 @@
 using namespace std;
 
 int main() {
+    int i;
+    int x[6] = {360, 0, 0, 360, 720, 720};
+    int y[6] = {0, 426, 853, 1280, 853, 426};
+
+
     // inicializar libreria SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         cout << "SDL_Init Error:" << SDL_GetError << endl;
@@ -27,7 +32,7 @@ int main() {
     /*  Crea una ventana de 1280 x 720 (HD) centrada en el monitor
         y crea un handle a la ventana grafica llamada win
     */
-    SDL_Window *win = SDL_CreateWindow("Line Drawing Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("Ejemplo de una Linea USFQ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
     if (win == NULL) {
         cout << "SDL_CreateWindow Error:" << SDL_GetError() << endl;
         SDL_Quit();
@@ -53,10 +58,16 @@ int main() {
     // Set the draw color to black for the line
     SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 
-    // Dibuja una linea que va desde (x1,y1) a (x2,y2)
-    SDL_RenderDrawLine(ren, 100, 100, 500, 300);
+    for(i = 0; i<5; i++){
+        SDL_RenderDrawLine(ren, y[i], x[i], y[i+1], x[i+1]);
+        SDL_RenderPresent(ren);
+    }
 
-    SDL_RenderPresent(ren);
+
+    // Dibuja una linea que va desde (x1,y1) a (x2,y2)
+    //SDL_RenderDrawLine(ren, 100, 100, 500, 300);
+    //SDL_RenderPresent(ren);
+
 
     SDL_Delay(10000); // Show the window for 5 seconds
 
