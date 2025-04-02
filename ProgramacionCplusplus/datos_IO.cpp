5501321faa3040f716ad02d7c50ac794d34816c2
Alberto Sanchez <asanchez@usfq.edu.ec>
2025-04-02 14:49:17 -0500

    update

---
+++ ProgramacionCplusplus/datos_IO.cpp
@@ -0,0 +1,65 @@
+/*
+    Ejemplo del uso de fstream para leer y escribir en
+    archivos.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+float array_min(float a[], int n);
+float array_max(float a[], int n);
+
+int main(){
+    float datos[100];
+    float minimo = 0;
+    float maximo = 0;
+    string datos_char;
+    int i = 0;
+    ifstream readFile;
+    readFile.open("datos.txt");
+
+    while((!readFile.eof()) && (i < 100)){  // eof - end of file
+        readFile >> datos[i];
+        cout << datos[i];
+        i++;
+    }
+    readFile.close();
+
+    minimo = array_min(datos[],i);
+    maximo = array_max(datos[],i);
+    rango = maximo - minimo;
+    intervalo = rango / 10;
+    n = i;
+    for(i = 0; i <n;i++){
+        if((datos[i]>= minimo) && (datos[i]< (minimo+intervalo))){
+            bin[0]++;
+        }else if((datos[i]>= (minimo+intervalo)) && (datos[i]<= (minimo+2*intervalo))){
+            bin[1]++;
+        }else if((datos[i]>= (minimo+2*intervalo)) && (datos[i]<= (minimo+3*intervalo))){
+            bin[2]++;
+        }else if((datos[i]>= (minimo+3*intervalo)) && (datos[i]<= (minimo+4*intervalo))){
+            bin[3]++;
+        }else if((datos[i]>= (minimo+4*intervalo)) && (datos[i]<= (minimo+5*intervalo))){
+            bin[4]++;
+        }else if((datos[i]>= (minimo+5*intervalo)) && (datos[i]<= (minimo+6*intervalo))){
+            bin[5]++;
+        }else if((datos[i]>= (minimo+6*intervalo)) && (datos[i]<= (minimo+7*intervalo))){
+            bin[6]++;
+        }else if((datos[i]>= (minimo+7*intervalo)) && (datos[i]<= (minimo+8*intervalo))){
+            bin[7]++;
+        }else if((datos[i]>= (minimo+8*intervalo)) && (datos[i]<= (minimo+9*intervalo))){
+            bin[8]++;
+        }else if((datos[i]>= (minimo+9*intervalo)) && (datos[i]<= (minimo+10*intervalo))){
+            bin[9]++;
+        }
+    }
+
+    for(i=0;i<10;i++){
+        cout << bin[i] << endl;
+    }
+
+    return 0;
+
+}
