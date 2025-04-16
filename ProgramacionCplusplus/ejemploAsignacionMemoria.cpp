6950303548090f3edbff068829f51a62fb1c78af
Alberto Sanchez <asanchez@usfq.edu.ec>
2025-04-16 15:25:12 -0500

    asignacion dinamica

---
+++ ProgramacionCplusplus/ejemploAsignacionMemoria.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float * ptrDato;
+    int i, n;
+
+    cout <<"Ingrese el numero de datos: ";
+    cin >> n;
+    cout << endl;
+
+    ptrDato = new float[n];
+
+    for(i=0;i<n;i++){
+        cout << "Ingrese el dato " << i;
+        cin >> *ptrDato;
+        ptrDato++;
+    }
+
+    for(i=n-1;i>=0;i--){
+        ptrDato--;
+        cout << "El dato " << i << " es " << *ptrDato <<endl; 
+    }
+
+    delete[] ptrDato;
+    return 0;
+}
